e450dcd65b6bca476cc8812d6d82152f91b55ab6
Michaël Dayen <dayen.michael@gmail.com>
2015-03-30 10:52:06 -0700

    initial commit

--- app/src/main/java/com/example/michaldayen/katchup2/MainActivity.java
+++ app/src/main/java/com/example/michaldayen/katchup2/MainActivity.java
@@ -21,8 +21,7 @@ public class MainActivity extends Activity {
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
+                "OS/2", "Ubuntu", "Windows7" };
 
         final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; ++i) {
